d48bf8e2c5ac51fe42bc1350a58c05b5047d0444
Kenny Dang <kenny@numerade.com>
2022-01-10 08:14:19 -0800

    Initial commit

---
+++ RickAndMortyDebugging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyDebugging
+//
+//  Created by Kenny Dang on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
